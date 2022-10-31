b2ff789c8e0be38e6ad0af6b4b3433e6a174e461
Regis Leandro Sebastiani <regisls@gmail.com>
2022-10-31 17:55:36 -0300

    Receitas e despesas por mês

--- src/components/Telas/Lancamento.tsx
+++ src/components/Telas/Lancamento.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 import {connect} from 'react-redux';
 
 import {listarPessoas} from '../../store/actions/autenticacaoActions';
@@ -7,34 +7,76 @@ import {
   listarLancamentosAno,
   modificarAnoReferencia,
 } from '../../store/actions/lancamentoActions';
+import mesExtenso from '../NomeMes';
+
+var result = [];
 
 const Lancamento = props => {
-  //const ano = props.modificarAnoReferencia;
-  
+  const agruparDados = () => {
+    props.lancamentos.forEach(lacto => {
+      if (!result[lacto.mes]) {
+        result[lacto.mes] = {
+          mes: lacto.mes,
+          despesas: lacto.tipo === 'D' ? +lacto.valor : 0,
+          receitas: lacto.tipo === 'C' ? +lacto.valor : 0,
+        };
+      } else {
+        result[lacto.mes] = {
+          mes: lacto.mes,
+          despesas: result[lacto.mes].despesas + (lacto.tipo === 'D' ? +lacto.valor : 0),
+          receitas: result[lacto.mes].receitas + (lacto.tipo === 'C' ? +lacto.valor : 0),
+        };
+      }
+    });
+  };
+
   useEffect(() => {
     (async () => {
-      props.listarPessoas();
-      props.listarLancamentosAno(props.anoReferencia);
+      await props.listarLancamentosAno(props.anoReferencia);
+      agruparDados();
     })();
   }, []);
 
   return (
     <View>
-      <Text>Pessoas</Text>
-      {props.pessoas.map(p => (
-        <Text key={p.id}>{p.nome}</Text>
-      ))}
-      <Text>
-        Lancamentos - {props.lancamentos.length}
-      </Text>
-      {props.lancamentos.map(l => (
-        <Text key={l.id}>
-          {l.valor.toString()} - {l.tipo} - {l.ano} - {l.mes}
-        </Text>
+      {result.map((l, i) => (
+        <View key={i} style={styles.container}>
+          <Text key={l.mes} style={styles.textBlack}>{mesExtenso(+l.mes)}</Text>
+          <Text key={`${l.mes}-R`} style={styles.textRed}>R$ {l.receitas.toFixed(2)}</Text>
+          <Text key={`${l.mes}-D`} style={styles.textBlue}>R$ {l.despesas.toFixed(2)}</Text>
+        </View>
       ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#bbbbbb',
+    marginRight: 10,
+    marginLeft: 10,
+    marginTop: 10,
+    padding: 10,
+    fontSize: 15,
+    flexDirection: 'row',
+    fustifyContent: 'space-between',
+  },
+  textRed: {
+    color: '#ff0000',
+    alignSelf: 'flex-end',
+    flex: 1,
+  },
+  textBlue: {
+    color: '#00ff00',
+    alignSelf: 'flex-end',
+    flex: 1,
+  },
+  textBlack: {
+    color: '#000000',
+    alignSelf: 'flex-start',
+    flex: 1,
+  },
+});
 
 const mapStateToProps = state => ({
   pessoas: state.autenticacao.pessoas,
